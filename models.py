9329f18be6819c5679c0e883c42d838408f94c7d
Quang <trandinhquang2005@gmail.com>
2026-04-15 21:55:00 +0700

    Fix relative import in models

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, Numeric, DateTime, Float
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from .database import Base
+from database import Base
 
 class Category(Base):
     __tablename__ = "categories"
