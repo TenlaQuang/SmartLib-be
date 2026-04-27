cfa934bc9c9296274c32a1a471a11862cb1e1f60
Quang <trandinhquang2005@gmail.com>
2026-04-27 21:31:50 +0700

    fix main

--- main.py
+++ main.py
@@ -54,6 +54,14 @@ static_dir = "static/images"
 os.makedirs(static_dir, exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Setup CORS for flutter web / local testing
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.middleware("http")
 async def add_cors_header(request, call_next):
